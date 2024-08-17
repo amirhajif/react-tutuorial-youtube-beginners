2d30fe81f0f667f7b521750221fd60e864629b5d
amirhossein hajitabar <amirhajitabar2@gmail.com>
2024-08-17 13:01:08 +0330

    add component example

---
+++ component-example/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Card() {
+    return (
+        <div className="card">
+            <div className="card-border-top">
+            </div>
+            <div className="img">
+            </div>
+            <span> Person</span>
+            <p className="job"> Job Title</p>
+            <button> Click
+            </button>
+        </div>
+    )
+}
---
+++ component-example/src/components/Cards.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './Card'
+
+export default function Cards() {
+    return (
+        <div className='container'>
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    )
+}
