f7fa3c46659231d44ced34c1a675ddaa28dd0722
amirhossein hajitabar <amirhajitabar2@gmail.com>
2024-09-14 18:19:35 +0330

    Add Children

---
+++ children/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Button from './components/Button'
+function App() {
+  return (
+    <Button>
+      <a href="https://www.webioo.ir">bezan brim</a>
+    </Button>
+  )
+}
+
+export default App
